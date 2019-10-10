efac6be87340846c8072b7b178ef9667abc02864
ipek <ipekcivicakan@gmail.com>
2019-10-10 18:00:43 -0400

    closure exercise one

--- closure-exercises.js
+++ closure-exercises.js
@@ -1,3 +1,5 @@
+// First Exercise
+
 let view;
 function initialize() {
     view = 'ocean';
@@ -8,3 +10,36 @@ initialize();
 initialize();
 initialize();
 console.log(view);
+
+// Use closure and don't initialize view everytime I call.
+
+//Solution 1
+let view;
+let initialize = () => {
+    view = 'ocean';
+    console.log('has been set');
+    return initialize = () => {
+        console.log('has already been set')
+    }
+}
+
+
+initalize()
+initalize()
+
+// Solution 2
+
+let view;
+function initalize3() {
+    let called = 0;
+    return function () {
+        if (called === 0) {
+            view = 'ocean';
+            console.log('has been set')
+            called++;
+            return view;
+        } else {
+            console.log('has already been set!!')
+        }
+    }
+}
