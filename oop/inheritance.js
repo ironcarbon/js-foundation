1c119138f22e802c02edba1f2338449c599aefbb
ipek <ipekcivicakan@gmail.com>
2019-10-15 22:13:05 -0400

    polymorphism

--- oop/inheritance.js
+++ oop/inheritance.js
@@ -51,3 +51,19 @@ console.log(dolby instanceof Character) //true
 // * Inheritance which is what we do with the keyword EXTENDS is inheriting something from a higher class. Inheritance in JS does not actually copy the functionality. Instead it simply links up the prototype chain.  EXTENDS => INHERIT
 
 //In JS, objects inherits objects. Class does not mean anything different in JS. These are just OBJECTS.
+
+
+
+
+
+
+//POLYMORPHISM
+// The idea is the ability to call the same method on different objects and each object responding in different way.
+
+//The idea of ability to appear in many forms that is we're simply doing method overwritting. Same method acts different;y for each type of class.
+
+// It has the ability to process object differently depending in their data type or class.
+
+//JS is a dynamically typed language, it actually limits the amount of polymorphisim that we can have but the idea is still same.
+
+//The ability to redefine methods for derived classes in allowing us to reuse some of the functionality but also customize methods to their own objects and classes and polymorphism is useful because we don't have to necessarily copy and paste code over and over. We can reuse some of the functionality from a superclass to adapt to our own specific needs 
