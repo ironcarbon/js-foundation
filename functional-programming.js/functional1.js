726b65f74af9ecff8644b70d3b6d497fe384c169
ipek <ipekcivicakan@gmail.com>
2019-10-16 12:35:49 -0400

    functional 1

---
+++ functional-programming.js/functional1.js
@@ -0,0 +1,6 @@
+// Functional Programming
+// Packaging the into separate chunks so that everything's well organized in each part of the code.
+// The idea of functional programming is separation between data over a program and the behaviour of a program.
+//In all objects created in functional programming are immutable! It means once something is created, it can not be changed.
+// Avoid shared state! Main principle is PURE FUNCTIONS
+ // Functional programming has so many restraints so it does not let the code get out of hand.
