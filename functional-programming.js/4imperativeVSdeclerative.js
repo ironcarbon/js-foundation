30cc501dc029d685ebba9ba2a197a4638d01ec42
ipek <ipekcivicakan@gmail.com>
2019-10-16 17:51:24 -0400

    4 imperative vs declarative

---
+++ functional-programming.js/4imperativeVSdeclerative.js
@@ -0,0 +1,19 @@
+// Imperative vs Declarative
+// Imperative code: tells the machine what to do and how to do it
+//Declarative code: tells what to do and what should happen. It does not tell the computer how to do things.
+
+// Machine code is very imperative.(where to put the variable in the memory space, then take it out somewhere, then modify)Very descriptive of how to do things. Versus Higher level languages become more declarative. We don't say where it should store the memory. We don't say how to do.
+
+// Examples
+
+for (let i = 0; i < 100; i++) {
+    console.log(i)
+}  // imperative way
+
+[1, 2, 3, 4].forEach(item => console.log(item)) //declarative way
+
+  //JQuery is a lot more imperative than front end frameworks like react, angular
+
+  //Declarative code is always going to end up either compiling down or being processed by something imperative like machine code.
+
+  //Declerative code is easier to read.
