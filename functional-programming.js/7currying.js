a66f65e6e404fa502e61042f9bd73572c2d89ca4
ipek <ipekcivicakan@gmail.com>
2019-10-17 13:30:02 -0400

    arity

--- functional-programming.js/7currying.js
+++ functional-programming.js/7currying.js
@@ -16,3 +16,10 @@ const curriedMultiplyBy5 = curriedMultiply(5); curriedMultiplyBy5(4);
 //Trying to save memory
 
 // Thanks to the closure, a is remembered by b function.
+
+
+//Arity
+//Number of arguments a function takes.
+// In functional programming, it is not a solid rule but it is usually a good practice that fewer number of parameters in a function is easier to use that function.
+
+// To make the funcitons more reusable in functional programming, use one or two parameters.
