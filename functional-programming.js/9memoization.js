3aa98188d930a1a99acbb60d4efa5116dc90abb1
ipek <ipekcivicakan@gmail.com>
2019-10-17 12:31:15 -0400

    memoization 2

--- functional-programming.js/9memoization.js
+++ functional-programming.js/9memoization.js
@@ -25,6 +25,32 @@ function memoizedAddTo500(n) {
 console.log('1', memoizedAddTo500(50));
 console.log('2', memoizedAddTo500(50));
 
-  // Memoization is a specific form of caching that involves caching return value of a function.
+// Memoization is a specific form of caching that involves caching return value of a function.
 
-  // Memoziation is simply a way to remember a solution to solve the proble
+// Memoziation is simply a way to remember a solution to solve the proble
+
+
+
+//Ideally, we don't want to fill the cache in global scope. Also, lives outside of function. Ideally, good practice is having memory or cache to live inside of the function not polluting the global scope. In JS, we can use CLOSURES.
+
+//Thanks to the closure, we are able to access cache variable inside of the child function.
+
+function memoizedAddTo500() {
+    let cache = {};
+    return function (n) {
+        if (n in cache) {
+            return cache[n];
+        } else {
+            console.log('takes time');
+            cache[n] = n + 500;
+            return cache[n];
+        }
+    }
+}
+
+const memoized = memoizedAddTo500();
+
+console.log('1', memoized(50));
+console.log('2', memoized(50));
+
+  // This allows us to be very efficient with our code.
